b20cd92afc8d4f6d85e3b3df0ad4dac9df182f69
Anithasrini <sanitha32@gmail.com>
2016-01-07 22:53:33 -0800

    Add spark to the project file

--- src/main/java/Leetspeak.java
+++ src/main/java/Leetspeak.java
@@ -18,14 +18,21 @@ public class Leetspeak {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-  }
 
-  public static String toLeetSpeak(String word) {
-    //
-    // "summer is awesome"
-    // word.charAt(0)
-    // String newWord;
 
+  get("/detector", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+     model.put("template", "templates/detector.vtl");
+
+     String word = request.queryParams("word");
+     String LeetSpeak =  Leetspeak(word);
+
+     model.put("LeetSpeak", Leetspeak);
+     model.put("word", request.queryParams("word"));
+     return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+ }
+  public static String LeetSpeak(String word) {
     if(word.contains("e")) {
       word = word.replace("e", "3");
     }
@@ -36,13 +43,9 @@ public class Leetspeak {
       word = word.replace("I", "1");
     }
 
-    //cut off first letter of String
-    //replace all s's with z's
-    //return new string and add first character back to the start of the string
 
-
-      String str = "Susie Sunshine";
-      str = str.substring (0);
+    //  String str = "Susie Sunshine";
+    //   str = str.substring (0);
 
     if(word.contains("s"))  {
       word = word.replace("s", "z");
--- src/test/java/LeetspeakTest.java
+++ src/test/java/LeetspeakTest.java
@@ -5,32 +5,27 @@ public class LeetspeakTest {
 
   @Test
   public void toLeetSpeak_replaceEInAStringTo3_wordChanges() {
-    Leetspeak leetspeak = new Leetspeak();
     assertEquals("tw33t", leetspeak.toLeetSpeak("tweet"));
   }
 
   @Test
   public void toLeetSpeak_replaceOInAStringTo0_wordChanges() {
-    Leetspeak leetspeak = new Leetspeak();
     assertEquals("c00l", leetspeak.toLeetSpeak("cool"));
   }
 
   @Test
   public void toLeetSpeak_replaceUppercaseIInAStringTo1_wordChanges() {
-    Leetspeak leetspeak = new Leetspeak();
     assertEquals("1ndia", leetspeak.toLeetSpeak("India"));
   }
 
   @Test
-  public void     toLeetSpeak_replaceAllInstancesOfSInAStringToZ_wordChanges() {
-    Leetspeak leetspeak = new Leetspeak();
+  public void toLeetSpeak_replaceAllInstancesOfSInAStringToZ_wordChanges() {
     assertEquals("r0z3z ar3 r3d", leetspeak.toLeetSpeak("roses are red"));
   }
 
   @Test
-  public void leetspeak_doesNotReplaceSWhenFirstLetter_wordChanges() {
-    Leetspeak leetspeak = new Leetspeak();
-  assertEquals("Suzi3 Sunzhin3", leetspeak.toLeetSpeak("Susie Sunshine"));
+  public void toleetspeak_doesNotReplaceSWhenFirstLetter_wordChanges() {
+    assertEquals("Suzi3 Sunzhin3", leetspeak.toLeetSpeak("Susie Sunshine"));
 }
 
   // @Test
